b6e643688d0550e5f1d2644c3f79cd451bc0b40b
Julio Cesar <juliozulu2008@gmail.com>
2023-11-08 10:44:18 -0300

    master

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -8,9 +8,9 @@ export function Description() {
         Olá, me chamo Júlio César, tenho 27 anos e sou do Paraná, Brasil.
         Analista de Sistemas e Analista de Suporte. Amo tecnologia, amo
         desenvolvimento de software, trabalho com TI desde os 15 anos. Tenho
-        esperiencia tanto com Infraestrutura como tambem com Desenvolvimento.
+        experiencia tanto com Infraestrutura como também com Desenvolvimento.
         Gosto de programar, tenho preferencia por WEB e MOBILE. Gosto de
-        Dasfios!
+        Desafios!
       </p>
     </article>
   );
